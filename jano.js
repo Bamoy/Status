347c8495690988887a8039fcc9f27660ea8ac259
Bamoy <79413044+Bamoy@users.noreply.github.com>
2022-08-18 13:22:04 +0300

    Create jano.js

---
+++ jano.js
@@ -0,0 +1,58 @@
+const Discord = require("discord.js");
+
+const jano = new Discord.Client();
+
+jano.on("message", message => {
+
+  if (!message.guild) return;
+
+  if (message.content === "wara") {
+
+    if (!message.author.id) return;
+
+    if (message.member.voiceChannel) {
+
+      message.member.voiceChannel
+
+        .join()
+
+        .then(connection => {
+
+          message.reply("**basha qashmar**");
+
+        })
+
+        .catch(console.log);
+
+    } else {
+
+      message.reply("**bro vc**");
+
+    }
+
+  }
+
+});
+
+jano.on("ready", () => {
+
+  console.log(`Online In Servers`);
+
+  let statuses = [`JUST ALLAH`];
+
+  setInterval(function() {
+
+    jano.login("")
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+
+    jano.user.setActivity(STREAMING, {
+
+      type: "playing",
+
+      url: "https://www.twitch.tv/faith"
+
+    });
+
+  }, 2000);
+
+});
